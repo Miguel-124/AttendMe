d94b194d1988bbca787020cc2a6e34225f1551ae
michalgorecki <michalgorecki124@gmail.com>
2025-02-03 23:02:18 +0100

    Index update

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../views/Login.vue';
 import TeacherDashboard from '../views/TeacherDashboard.vue';
 import StudentDashboard from '../views/StudentDashboard.vue';
+import SessionDetails from '../views/SessionDetails.vue';
+import RegisterDevice from '../views/RegisterDevice.vue';
 
 const routes = [
   { path: '/', component: Login },
   { path: '/teacher', component: TeacherDashboard },
-  { path: '/student', component: StudentDashboard }
+  { path: '/student', component: StudentDashboard },
+  { path: '/session/:id', component: SessionDetails },
+  { path: '/register-device', component: RegisterDevice }
 ];
 
 const router = createRouter({
