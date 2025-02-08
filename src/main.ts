160bd6707e1f8a635868127301b5efba2aa4e8f3
michalgorecki <michalgorecki124@gmail.com>
2025-02-08 22:52:28 +0100

    Update dependencies metadata and add console logs for app initialization

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 
+console.log("✅ Vue uruchomione!");
+
 const app = createApp(App);
 app.use(router);
 app.use(createPinia());
 app.mount("#app");
+
+console.log("✅ Aplikacja zamontowana!");
